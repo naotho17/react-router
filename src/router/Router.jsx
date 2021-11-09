99230466491715cff2bcc7eac0003c01764e281c
naotho17 <77154584+naotho17@users.noreply.github.com>
2021-11-09 18:25:47 +0900

    3

---
+++ src/router/Router.jsx
@@ -0,0 +1,36 @@
+import { Switch, Route } from "react-router-dom";
+import { Home } from "../Home";
+import { Page2 } from "../Page2";
+import { page1Routes } from "./Page1Routes";
+
+export const Router = () => {
+  return (
+    <Switch>
+      {/* Home */}
+      <Route exact path="/">
+        <Home />
+      </Route>
+      {/* Page1 */}
+      <Route
+        path="/page1"
+        render={({ match: { url } }) => (
+          <Switch>
+            {page1Routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      />
+      {/* Page2 */}
+      <Route path="/page2">
+        <Page2 />
+      </Route>
+    </Switch>
+  );
+};
